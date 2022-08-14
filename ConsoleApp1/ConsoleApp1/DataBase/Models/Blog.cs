2152caf17754506d67e72d92ab6f348b1c5cbce5
unknown <you@example.com>
2022-08-14 06:58:24 -0700

    Blog class create but have error

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/Blog.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp1.DataBase.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models
+{
+    public class Blog : Entity<string>
+    {
+
+        public User FromUser { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public BlogStatus BlogStatus { get; set; }
+        public DateTime CreatBlogCreateDate { get; set; } = DateTime.Now;
+
+        public Blog(User user, string title, string content, BlogStatus blogStatus, string id = null)
+        {
+
+            FromUser = user;
+            Title = title;
+            Content = content;
+            BlogStatus = blogStatus;
+            if (id != null)
+            {
+                Id = id;
+            }
+            else
+            {
+                Id = BlogRepo.RandomCode;
+            }
+        }
+        public string GetInfo()
+        {
+            return $"<BlogCreateDate : {CreatBlogCreateDate}>] [<blogCode : {Id}>] [<Title : {Title}>] [<Content : {Content}>] [<BlogStatus : {BlogStatus}>]";
+        }
+        public string GetInfoSended()
+        {
+            return $"[<PublishDate(dd.MM.yyyy) : {CreatBlogCreateDate}] [<BlogCode : {Id}>] [<BlogStatus> : {BlogStatus}] [<FirstName : {FromUser.Name}> <LastName : {FromUser.LastName}>] \n {Title} \n{Content}";
+        }
+    }
+}
