234e29773dd5e22c70da097ec99088da4a04eb06
unknown <you@example.com>
2022-08-14 07:16:38 -0700

    Created Validation file and Common Methood for count

---
+++ ConsoleApp1/ConsoleApp1/AppLogic/Validations/CommoonValidations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.AppLogic.Validations
+{
+    public class CommoonValidations
+    {
+
+        public static bool Count(string text, Predicate<int> countLogic)
+        {
+            if (countLogic(text.Length))
+            {
+                return true;
+            }
+            Console.WriteLine("Lentgh is not correct");
+            return false;
+        }
+
+    }
+}
