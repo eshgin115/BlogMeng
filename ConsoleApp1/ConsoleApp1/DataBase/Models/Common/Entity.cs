694fc11c661f0c0c9752a68eea0627cc79b13875
unknown <you@example.com>
2022-08-14 06:55:29 -0700

    Created Entity class

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/Common/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models.Common
+{
+    public abstract class Entity<T>
+    {
+        public T Id { get; set; }
+
+    }
+}
