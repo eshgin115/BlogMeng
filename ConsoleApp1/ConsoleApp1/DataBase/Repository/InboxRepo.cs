a8104196720a65adf5eb7ef07401cab863309fe2
unknown <you@example.com>
2022-08-14 07:11:37 -0700

    Created Inbox Repo

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Repository/InboxRepo.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1.DataBase.Models;
+using ConsoleApp1.DataBase.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Repository
+{
+    public class InboxRepo : Repo<Inbox, int>
+    {
+    }
+}
