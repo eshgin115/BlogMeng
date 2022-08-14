54a0060a612e1e216613994d07a376a3603b0767
unknown <you@example.com>
2022-08-14 07:03:43 -0700

    Created Inbox but have erorr

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/Inbox.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp1.DataBase.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models
+{
+    public class Inbox : Entity<int>
+    {
+        public string Notification { get; set; }
+        public User User { get; set; }
+
+        public Inbox(string notification, User user, int? id = null)
+        {
+            Notification = notification;
+            User = user;
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+            else
+            {
+                Id = UserRepo.IDCounter;
+            }
+
+
+        }
+    }
+}
