e4ee3fa110a06cf376da584d10adf8a0cf69f47a
unknown <you@example.com>
2022-08-14 07:01:04 -0700

    Created admin class inherence from users

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/Admin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models
+{
+    public class Admin : User
+    {
+        public Admin(string name, string lastName, string email, string password, int? id = null)
+            : base(name, lastName, email, password, id)
+        {
+        }
+
+        public override string GetInfo()
+        {
+            return Name + " " + LastName + " " + Email + " " + RigisterTime;
+        }
+    }
+}
