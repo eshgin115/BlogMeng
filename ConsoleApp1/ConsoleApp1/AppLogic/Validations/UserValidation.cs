aec2ab00aca38ce653c706e62d2996a13b78977c
unknown <you@example.com>
2022-08-14 07:18:03 -0700

    Created UserValidations class and methood for Register

---
+++ ConsoleApp1/ConsoleApp1/AppLogic/Validations/UserValidation.cs
@@ -0,0 +1,53 @@
+﻿using ConsoleApp1.DataBase.Models;
+using ConsoleApp1.DataBase.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.AppLogic.Validations
+{
+    public class UserValidation
+    {
+        public static List<User> users = new List<User>();
+        public static bool IsCheckName(string name)
+        {
+            if (Regex.IsMatch(name, @"^[A-Z]{1,1}[a-zA-Z]{3,30}"))
+            {
+                return true;
+            }
+            Console.WriteLine("Name Is not correcr");
+            return false;
+        }
+        public static bool IsCheckLastName(string lastName)
+        {
+            if (Regex.IsMatch(lastName, @"^[A-Z]{1,1}[a-zA-Z]{4,29}"))
+            {
+                return true;
+            }
+            Console.WriteLine("LastName is not correct");
+            return false;
+        }
+        public static bool IsCheckEmail(string email)
+        {
+            if (Regex.IsMatch(email, @"^[a-zA-Z0-9]{10,20}@code\.edu\.az") && UserRepo.IsEmailConfirmation(email))
+            {
+                return true;
+            }
+            Console.WriteLine("Your email is not correct or already exists");
+            return false;
+        }
+        public static bool IsCheckPass(string pass)
+        {
+            if (Regex.IsMatch(pass, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"))
+            {
+                return true;
+            }
+            Console.WriteLine("your password is not correct");
+            return false;
+
+        }
+    }
+}
