a8c60c456c571aedf3d08359196c7d7501b3662b
unknown <you@example.com>
2022-08-14 06:56:44 -0700

    Created User class

--- ConsoleApp1/ConsoleApp1/DataBase/Models/User.cs
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.DataBase.Models.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.DataBase.Models
 {
-    internal class User
+    public class User : Entity<int>
     {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public DateTime RigisterTime { get; set; } = DateTime.Now;
+        public User(string name, string lastName, string email, string password, int? id = null)
+        {
+            Name = name;
+            LastName = lastName;
+
+            Email = email;
+            Password = password;
+            if (id != null)
+            {
+                Id = id.Value;
+            }
+            else if (id == null)
+            {
+
+
+            }
+        }
+        public virtual string GetInfo()
+        {
+            return Name + " " + LastName + " " + Email + " " + Password;
+        }
     }
 }
