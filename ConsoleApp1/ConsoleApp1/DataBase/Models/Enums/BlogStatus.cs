be7e3a49c0283f7e95187cb1054cad4673bafe33
unknown <you@example.com>
2022-08-14 07:02:29 -0700

    Created enum (BlogStatus) for blog

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/Enums/BlogStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models.Enums
+{
+    public enum BlogStatus
+    {
+        Accepted = 1,
+        Rejected = 2,
+        Sended = 3,
+
+    }
+}
