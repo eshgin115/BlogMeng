e17e9de031a1c72958cc6347ac4412d9fda89fb7
unknown <you@example.com>
2022-08-14 07:13:30 -0700

    work has been done in Programm css

--- ConsoleApp1/ConsoleApp1/UI/Program.cs
+++ ConsoleApp1/ConsoleApp1/UI/Program.cs
@@ -6,7 +6,46 @@ namespace ConsoleApp1.UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("/Register");
+            Console.WriteLine("/Login");
+            Console.WriteLine("/exit");
+            Console.WriteLine("/show-blogs-with-comments");
+            Console.WriteLine("/show-filtered-blogs-with-comments");
+            Console.WriteLine("/find-blog-by-code");
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter command: ");
+                string command = Console.ReadLine();
+                if (command == "/Register")
+                {
+                    Authenfication.Register();
+                }
+                else if (command == "/Login")
+                {
+                    Authenfication.Login();
+                }
+                else if (command == "/exit")
+                {
+                    break;
+                }
+                else if (command == "/show-blogs-with-comments")
+                {
+                    ServicBlog.ShowBlog();
+                }
+                else if (command == "/show-filtered-blogs-with-comments")
+                {
+                    ServicBlog.ShowFilteredBlog();
+                }
+                else if (command == "/find-blog-by-code")
+                {
+                    ServicBlog.FindBlogByCode();
+                }
+                else
+                {
+                    Console.WriteLine("Command is not correct");
+                }
+            } 
         }
     }
 }
